f733d9b9a2748a636a7c9acfb12640e848628d36
Andy Williams <andy@andywilliams.me>
2018-10-22 17:30:51 +0100

    Print a helpful message rather than failing to compile

--- desk_other.go
+++ desk_other.go
@@ -2,6 +2,9 @@
 
 package desktop
 
+import "log"
+import "runtime"
+
 import "github.com/fyne-io/fyne"
 import _ "github.com/fyne-io/fyne/test"
 
@@ -11,6 +14,9 @@ func isEmbedded() bool {
 
 // newDesktopWindow creates a test window in memory for automated testing.
 func newDesktopWindow(app fyne.App) fyne.Window {
+	if runtime.GOOS != "linux" {
+		log.Println("Fyne Desktop currenly only works on Linux")
+	}
 	return app.NewWindow("Fyne Desktop")
 }
 
