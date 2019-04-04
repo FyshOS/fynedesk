a5036054810e8b97ed46b00684c80072d25d6095
Andy Williams <andy@andy.xyz>
2019-04-04 22:37:16 +0100

    Add a runner command for anyone worried about a crashing window manager

---
+++ cmd/fynedesk_runner/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+const runCmd = "fynedesk"
+
+func main() {
+	for {
+		exe := exec.Command(runCmd)
+		exe.Stdout = os.Stdout
+		exe.Stderr = os.Stderr
+		err := exe.Run()
+		if err == nil {
+			return
+		}
+	
+		exitErr, ok := err.(*exec.ExitError)
+		if !ok {
+			log.Println("Could not execute", runCmd, "command")
+			return
+		}
+
+		if status, ok := exitErr.Sys().(syscall.WaitStatus); ok && status == 0 {
+			log.Println("Exiting Error 0")
+			return
+		} else if status == 512 { // X server unavailable
+			log.Println("X server went away")
+			return
+		} else {
+			log.Println("Restart from status", status)
+		}
+	}
+}
