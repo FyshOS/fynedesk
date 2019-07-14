33fc1404b42c0252faa989ead3fd2e7eb871cff5
Andy Williams <andy@andy.xyz>
2019-07-14 21:39:56 +0100

    Oops, fix formatting

--- wm/border.go
+++ wm/border.go
@@ -10,7 +10,7 @@ import (
 
 func newBorder(title string) fyne.CanvasObject {
 	filler := canvas.NewRectangle(theme.BackgroundColor()) // make a border on the X axis only
-	filler.SetMinSize(fyne.NewSize(0, 2)) // 0 wide forced
+	filler.SetMinSize(fyne.NewSize(0, 2))                  // 0 wide forced
 	titleBar := widget.NewHBox(filler,
 		widget.NewButtonWithIcon("", theme.CancelIcon(), func() {}),
 		widget.NewLabel(title))
