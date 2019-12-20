041a03b0dd1cf53a83194b316d0cfbe4b0c452dd
Andy Williams <andy@andy.xyz>
2019-12-20 13:59:18 +0000

    Let's use the Fyne icon for FyneDesk windows like Settings

--- cmd/fynedesk/main.go
+++ cmd/fynedesk/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/theme"
 )
 
 func main() {
 	a := app.NewWithID("io.fyne.desktop")
+	a.SetIcon(theme.FyneLogo())
 	desk := setupDesktop(a)
 
 	desk.Run()
