9c2262966136effe1fc66333e40c7341b6deead7
Andy Williams <andy@andy.xyz>
2020-04-24 17:59:23 +0100

    Add basic API on top of DBus for advertising services

---
+++ wm/dbus.go
@@ -0,0 +1,46 @@
+package wm
+
+import (
+	"errors"
+
+	"github.com/godbus/dbus/v5"
+)
+
+var conn *dbus.Conn
+
+func connectBus() (*dbus.Conn, error) {
+	if conn != nil {
+		return conn, nil
+	}
+
+	c, err := dbus.SessionBus()
+	if err != nil {
+		return nil, err
+	}
+	// TODO cleanly shut down with conn.Close()
+
+	conn = c
+	return conn, nil
+}
+
+func RegisterService(obj interface{}, path, iface string) error {
+	conn, err := connectBus()
+	if err != nil {
+		return err
+	}
+
+	err = conn.Export(obj, dbus.ObjectPath(path), iface)
+	if err != nil {
+		return err
+	}
+
+	reply, err := conn.RequestName(iface, dbus.NameFlagDoNotQueue)
+	if err != nil {
+		return err
+	}
+	if reply != dbus.RequestNameReplyPrimaryOwner {
+		return errors.New("name already taken")
+	}
+
+	return nil
+}
