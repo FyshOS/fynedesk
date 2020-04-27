134a1b30a890af185ad5384f2302df57fb92cc5b
Andy Williams <andy@andy.xyz>
2020-04-27 09:35:06 +0100

    Default to scale of active screen

--- internal/x11/wm/screens.go
+++ internal/x11/wm/screens.go
@@ -83,7 +83,7 @@ func (xsp *x11ScreensProvider) ScreenForGeometry(x int, y int, width int, height
 			return xsp.screens[i]
 		}
 	}
-	return xsp.screens[0]
+	return xsp.active
 }
 
 func (xsp *x11ScreensProvider) ScreenForWindow(win fynedesk.Window) *fynedesk.Screen {
