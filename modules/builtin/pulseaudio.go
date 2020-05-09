bfdeff1211b3cf71faa3f6eea6fb6d8c962462db
Matthieu Bollot <m.bollot@code-troopers.com>
2020-05-10 00:08:59 +0200

    Replace aurora lib by a full go one

--- modules/builtin/pulseaudio.go
+++ modules/builtin/pulseaudio.go
@@ -1,15 +1,13 @@
 package builtin
 
 import (
-	"errors"
 	"log"
-	"time"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/layout"
 	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
-	"github.com/auroralaboratories/pulse"
+	"github.com/mattboll/pulseaudio"
 
 	"fyne.io/fynedesk"
 	wmtheme "fyne.io/fynedesk/theme"
@@ -23,42 +21,28 @@ var pulseaudioMeta = fynedesk.ModuleMetadata{
 // Pulseaudio is a progress bar module to modify screen pulseaudio
 type Pulseaudio struct {
 	bar       *widget.ProgressBar
-	client    *pulse.Client
+	client    *pulseaudio.Client
 	soundIcon *widget.Button
 }
 
 // Destroy destroys the module
 func (b *Pulseaudio) Destroy() {
-	b.client.Destroy()
+	b.client.Close()
 }
 
-func (b *Pulseaudio) value() (float64, bool, error) {
-	sinks, err := b.client.GetSinks()
+func (b *Pulseaudio) value() (float32, bool, error) {
+	volume, err := b.client.Volume()
 	if err != nil {
 		return 0, true, err
 	}
-	if len(sinks) <= 0 {
-		return 0, true, errors.New("no sinks returned")
-	}
 
-	sink := sinks[0]
-	if err := sink.Refresh(); err != nil {
-		return 0, true, err
-	}
-
-	return sink.VolumeFactor, sink.Muted, nil
+	return volume, false, nil
 }
 
 // OffsetValue actually increase or decrease the screen pulseaudio
 func (b *Pulseaudio) OffsetValue(diff int) {
-	sinks, err := b.client.GetSinks()
-	if err != nil || len(sinks) <= 0 {
-		log.Println("GetSinks() failed", err)
-		return
-	}
-
 	floatVal, _, _ := b.value()
-	value := floatVal + float64(diff)/100
+	value := floatVal + float32(diff)/100
 
 	if value < 0 {
 		value = 0
@@ -66,42 +50,33 @@ func (b *Pulseaudio) OffsetValue(diff int) {
 		value = 1
 	}
 
-	for _, sink := range sinks {
-		if err := sink.SetVolume(value); err != nil {
-			log.Println("Failed to set volume", err)
-			return
-		}
+	if err := b.client.SetVolume(value); err != nil {
+		log.Println("Failed to set volume", err)
+		return
 	}
 
-	b.bar.SetValue(value)
+	b.bar.SetValue(float64(value))
 }
 
 // ToggleMute is used to switch between mute or not
 func (b *Pulseaudio) ToggleMute() {
-	sinks, err := b.client.GetSinks()
-	if err != nil || len(sinks) <= 0 {
-		log.Println("GetSinks() failed", err)
+	toggl, err := b.client.ToggleMute()
+	if err != nil {
+		log.Println("ToggleMute() failed", err)
 		return
 	}
 
-	for _, sink := range sinks {
-		err = sink.ToggleMute()
-		if err != nil {
-			log.Println("ToggleMute() failed", err)
-		}
-
-		if sink.Muted {
-			b.soundIcon.SetIcon(wmtheme.MuteIcon)
-		} else {
-			b.soundIcon.SetIcon(wmtheme.SoundIcon)
-		}
+	if toggl {
+		b.soundIcon.SetIcon(wmtheme.MuteIcon)
+	} else {
+		b.soundIcon.SetIcon(wmtheme.SoundIcon)
 	}
 
 }
 
 // StatusAreaWidget builds the widget
 func (b *Pulseaudio) StatusAreaWidget() fyne.CanvasObject {
-	client, err := pulse.NewClient(`fyne-client` + string(time.Now().Unix()))
+	client, err := pulseaudio.NewClient()
 	if err != nil {
 		return nil
 	}
