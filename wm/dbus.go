a76120a4bf86d83e5c4e9ae905f68f540de4cca5
Andy Williams <andy@andy.xyz>
2020-05-14 19:49:33 +0100

    Tidy DBus and add new CallMethod helper

--- wm/dbus.go
+++ wm/dbus.go
@@ -3,24 +3,36 @@ package wm
 import (
 	"errors"
 
+	"fyne.io/fyne"
+
 	"github.com/godbus/dbus/v5"
 )
 
-var conn *dbus.Conn
-
-func connectBus() (*dbus.Conn, error) {
-	if conn != nil {
-		return conn, nil
-	}
-
-	c, err := dbus.SessionBus()
+// CallMethod simplifies calling a function on the DBus message system.
+// The name of the method and its interface and path are passed as strings.
+// The in parameters must be in the correct number and type to match the
+// requested method and the returned parameters will be returned from this
+// method. If an error occurred the last return parameter will be set.
+func CallMethod(in []interface{}, path, iface, meth string) ([]interface{}, error) {
+	conn, err := dbus.ConnectSessionBus()
 	if err != nil {
+		fyne.LogError("Error opening DBus connection", err)
 		return nil, err
 	}
-	// TODO cleanly shut down with conn.Close()
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			fyne.LogError("Error closing DBus connection", err)
+		}
+	}()
 
-	conn = c
-	return conn, nil
+	obj := conn.Object(iface, dbus.ObjectPath(path))
+	call := obj.Call(meth, 0, in...)
+	if call.Err != nil {
+		return nil, call.Err
+	}
+
+	return call.Body, nil
 }
 
 // RegisterService allows an object to be exported to the DBus messaging system.
@@ -28,8 +40,9 @@ func connectBus() (*dbus.Conn, error) {
 // return types, in which case a non-nil error will send an error message
 // instead of the object response.
 func RegisterService(obj interface{}, path, iface string) error {
-	conn, err := connectBus()
+	conn, err := dbus.SessionBus()
 	if err != nil {
+		fyne.LogError("Error accessing to shared DBus connection", err)
 		return err
 	}
 
