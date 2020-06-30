9c84090b0784354a88e627e617d518744c15abb1
Andy Williams <andy@andy.xyz>
2020-06-30 21:45:23 +0100

    Copy log files out to crash logs when something goes wrong

--- cmd/fynedesk_runner/log.go
+++ cmd/fynedesk_runner/log.go
@@ -1,34 +1,49 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 
 	"fyne.io/fyne"
 )
 
-func logPath() string {
-	return logPathRelativeTo(systemLogDir())
+func crashLogPath() string {
+	return crashLogPathRelativeTo(systemLogDir())
 }
 
-func logPathRelativeTo(parent string) string {
+func crashLogPathRelativeTo(parent string) string {
+	path := filepath.Join(logDir(parent), "fynedesk")
+	now := time.Now().Format(time.RFC3339)
+	return fmt.Sprintf("%s-crash-%s.log", path, now)
+}
+
+func logDir(parent string) string {
 	cacheDir := filepath.Join(parent, "fyne", "io.fyne.fynedesk")
 	err := os.MkdirAll(cacheDir, 0700)
 	if err != nil {
 		fyne.LogError("Could not create log directory", err)
 	}
 
-	return filepath.Join(cacheDir, "fynedesk.log")
+	return cacheDir
 }
 
-// openLogWriter returns a stream for stdOut and stdErr of the process we run
-func openLogWriter() (io.WriteCloser, io.WriteCloser) {
+func logPath() string {
+	return logPathRelativeTo(systemLogDir())
+}
+
+func logPathRelativeTo(parent string) string {
+	return filepath.Join(logDir(parent), "fynedesk.log")
+}
+
+// openLogWriter returns the log file for stdOut and stdErr of the process we run
+func openLogWriter() *os.File {
 	f, err := os.Create(logPath())
 	if err != nil {
 		fyne.LogError("Unable to open log file", err)
-		return os.Stdout, os.Stderr
+		return os.Stderr
 	}
 
-	return f, f
+	return f
 }
