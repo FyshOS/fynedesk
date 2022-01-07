ff69bb9180aec8995916a6e8c44b666b97106d72
Andy Williams <andy@fyne.io>
2022-01-06 16:51:12 -0800

    Fix indentation - will go check my git hooks

--- internal/x11/win/frame.go
+++ internal/x11/win/frame.go
@@ -463,7 +463,7 @@ func (f *frame) hide() {
 
 	borderWidth := x11.BorderWidth(x11.XWin(f.client))
 	titleHeight := x11.TitleHeight(x11.XWin(f.client))
-	x, y := f.x + int16(borderWidth), f.y + int16(titleHeight)
+	x, y := f.x+int16(borderWidth), f.y+int16(titleHeight)
 	xproto.ReparentWindow(f.client.wm.Conn(), f.client.win, f.client.wm.X().RootWin(), x, y)
 	xproto.UnmapWindow(f.client.wm.Conn(), f.client.win)
 }
