e92cf8d5d1159db901075cfcb14cbdcf3516864f
Andy Williams <andy@andy.xyz>
2022-12-27 19:33:16 +0000

    Add some tests missing

---
+++ wm/shortcut_test.go
@@ -0,0 +1,28 @@
+package wm
+
+import (
+	"testing"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fynedesk"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShortcutHandler_Shortcuts(t *testing.T) {
+	m := &ShortcutHandler{}
+	assert.Equal(t, 0, len(m.Shortcuts()))
+
+	m.AddShortcut(fynedesk.NewShortcut("Hint", fyne.KeyB, fyne.KeyModifierSuper), func() {})
+	assert.Equal(t, 1, len(m.Shortcuts()))
+}
+
+func TestShortcutHandler_TypedShortcut(t *testing.T) {
+	m := &ShortcutHandler{}
+	called := false
+	key := fynedesk.NewShortcut("Hint", fyne.KeyH, fyne.KeyModifierSuper)
+	m.AddShortcut(key, func() {
+		called = true
+	})
+	m.TypedShortcut(key)
+	assert.True(t, called)
+}
---
+++ wm/util_test.go
@@ -0,0 +1,32 @@
+package wm
+
+import (
+	"testing"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/test"
+	"fyne.io/fyne/v2/widget"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindObjectAtPixelPositionMatching(t *testing.T) {
+	l := widget.NewLabel("Test")
+	e := widget.NewEntry()
+	w := test.NewWindow(
+		container.NewGridWithColumns(1, l, e))
+
+	assert.Equal(t, l, FindObjectAtPixelPositionMatching(8, 8, w.Canvas(), func(fyne.CanvasObject) bool {
+		return true
+	}))
+	assert.Equal(t, e, FindObjectAtPixelPositionMatching(8, 52, w.Canvas(), func(o fyne.CanvasObject) bool {
+		_, ok := o.(*widget.Entry)
+		return ok
+	}))
+	assert.Nil(t, FindObjectAtPixelPositionMatching(68, 68, w.Canvas(), func(fyne.CanvasObject) bool {
+		return true
+	}))
+	assert.Nil(t, FindObjectAtPixelPositionMatching(8, 8, w.Canvas(), func(fyne.CanvasObject) bool {
+		return false
+	}))
+}
