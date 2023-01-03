794fde54b56080640e2cc2d51ae90ece31ab9c11
Andy Williams <andy@fyne.io>
2023-01-03 15:34:53 +0000

    Add network and systray to default modules

--- internal/ui/settings.go
+++ internal/ui/settings.go
@@ -233,7 +233,7 @@ func (d *deskSettings) load() {
 		d.launcherZoomScale = 2.0
 	}
 
-	moduleNames := fyne.CurrentApp().Preferences().StringWithFallback("modulenames", "Battery|Brightness|Compositor|Sound|Launcher: Calculate|Launcher: Open URLs")
+	moduleNames := fyne.CurrentApp().Preferences().StringWithFallback("modulenames", "Battery|Brightness|Compositor|Sound|Launcher: Calculate|Launcher: Open URLs|Network|SystemTray")
 	if moduleNames != "" {
 		d.moduleNames = strings.Split(moduleNames, "|")
 	}
